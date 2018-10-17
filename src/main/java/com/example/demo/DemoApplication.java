c8842e0b195e3c04ddd9c12a9bedd8b8c7860926
Winiton.c <winiton.chanapolchai@integrationworks.co.th>
2018-10-17 15:47:05 +0700

    comment more

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         //Test!!!
+        // Test From Chaow
         SpringApplication.run(DemoApplication.class, args);
     }
 
