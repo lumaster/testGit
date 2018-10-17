8a40e432975d12cd9044fe262e23c6fd804dab03
Naraporn Sae - saen <saennara91@gmail.com>
2018-10-17 15:34:58 +0700

    Task one done!!!

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+
     public static void main(String[] args) {
+        //Test!!!
         SpringApplication.run(DemoApplication.class, args);
     }
+
 }
